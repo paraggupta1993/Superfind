79dab895c9de8e4e4a5851ad2432083b358aa148
Parag Gupta <paraggupta1993@gmail.com>
2014-09-04 17:54:08 +0530

    initial loader untested

---
+++ src/bookmarklet.js
@@ -0,0 +1,9 @@
+javascript:
+(
+  function(){
+    var scriptNode =document.createElement('script');
+    scriptNode.type='text/javascript';
+    scriptNode.src='';
+    document.body.appendChild(scriptNode);
+  }
+)();
