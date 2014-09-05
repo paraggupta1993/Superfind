dff7991e71830ef2a0515b717e04f46f36fbeb4a
Parag Gupta <paraggupta1993@gmail.com>
2014-09-05 18:09:37 +0530

    Initial Words extractor using jquery

--- javascripts/superfind.js
+++ javascripts/superfind.js
@@ -1,3 +1,49 @@
 (function(){
-  alert("Welcome to SuperFind");
+
+  main = function(){
+    alert("Welcome to SuperFind");
+    loadJquery();
+
+    // Start polling...
+    checkReady(function($) {
+      $(function() {
+        //After Loading Jquery :
+
+        dic = wordsExtractor($);
+        console.log(dic);
+
+      });
+    });
+  }
+
+  wordsExtractor = function($){
+  //Thanks : http://stackoverflow.com/questions/16906440/how-to-get-an-array-of-all-words-used-on-a-page
+    return res =
+      $('body  *')
+      .contents()
+      .map(function () {
+        if (this.nodeType == 3 && this.nodeValue.trim() != ""){//check for nodetype text and ignore empty text nodes
+          return this.nodeValue.trim().split(/\W+/);  //split the nodevalue to get words.
+        }
+      }).get(); //get the array of words.
+  }
+
+  loadJquery = function(){
+    // Thanks : anders.tornblad@gmail.com
+    var script = document.createElement("SCRIPT");
+    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName("head")[0].appendChild(script);
+  }
+  // Poll for jQuery to come into existance
+  checkReady = function(callback) {
+      if (window.jQuery) {
+          callback(jQuery);
+      }
+      else {
+          window.setTimeout(function() { checkReady(callback); }, 20);
+      }
+  };
+
+  main();
 })();
